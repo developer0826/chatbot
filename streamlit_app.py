b2659fe811714ad1e4a3a256b3c25b9d7ec63746
GU Seona <seona_0826@naver.com>
2024-08-23 17:45:14 +0900

    240823_3rd_project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,52 @@
 import streamlit as st
-from openai import OpenAI
+import time
+from model import response_from_llm
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+# model 모듈 사용하여 응답 받아오는 부분
+# Streamlit 응답 타이핑 시각화
+def response_generator(user_text):
+    session_id = get_script_run_ctx().session_id
+    response = response_from_llm(user_text, session_id)
+    for line in response.split('\n'):
+        for word in line.split():
+            yield word + " "
+            time.sleep(0.05)
+        yield "  \n"
+
+# Streamlit Page
+st.set_page_config(
+    page_title="SK매직 챗봇",
+    page_icon="😊",
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+st.image("./data/logo.jpg", width=100)
+st.subheader("에코미니 정수기 사용법을 알려드려요😊")
+st.caption("궁금한 내용을 자유롭게 물어보세요!")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar=message["avatar"]):
+        if message["role"] == "user":
             st.markdown(message["content"])
+        else:
+            with st.status("답변을 확인하세요.", expanded=False, state="complete"):
+                st.write(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("어떤 것이 궁금하세요?"):
+    st.session_state.messages.append({"role": "user", "avatar":"🙋🏻", "content": prompt})
+    with st.chat_message(name="user", avatar="🙋🏻"):
+        st.markdown(prompt)
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.chat_message(name="assistant", avatar="🧑🏻‍🔧"):
+        with st.status("응답 중입니다.", expanded=True) as status:
+            response = st.write_stream(response_generator(prompt))
+            status.update(
+                label="답변을 확인하세요.", state="complete", expanded=True
+            )   
+        
+    st.session_state.messages.append({"role": "assistant", "avatar":"🧑🏻‍🔧", "content": response})
